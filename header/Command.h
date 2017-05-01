a6d71f4132b5a7428ee524c9a856a7343ff44205
JKaneshiro <jkaneshiro@ucrwpa1-3-10-25-10-218.wnet.ucr.edu>
2017-05-01 13:15:34 -0700

    added fn declarations for Command.h

--- header/Command.h
+++ header/Command.h
@@ -1,10 +1,17 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include "Shell.h" 
+#include <iostream>
+#include "Shell.h"
 
 Class Command : public Shell {
-
+private:
+    char* cmd[];
+public:
+    Command();
+    void parse();
+    void read();
+    void execute();
 };
 
 #endif
