1535b33337bccf866c65fd4232c0e6e196a95641
Acruz029 <andrewcruz1000@gmail.com>
2017-05-31 22:10:51 -0700

    Stuff

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -10,5 +10,6 @@ ls -l || (echo this && echo should) && (echo work);
 (echo A || echo B) && ls -l || (echo C && echo D)
 (echo A || echo B) && ls -l
 (echo A || echo B) && ls -l || (echo C && echo D) && echo hi
+
 exit
 make clean
